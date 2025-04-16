02036c0878132bb83913986586f6c67ce123b76e
2648623608 <2648623608@qq.com>
2025-04-16 16:24:59 +0800

    111

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def getRes():
+    input_text = request.args.get('sendtext')
+    url = "https://f8b2-220-197-4-72.ngrok-free.app/"
+    params = {
+        "sendtext": input_text
+    }
+    response = requests.get(url, params=params)
+    return response.text
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
